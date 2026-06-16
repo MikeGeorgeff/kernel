a9d408413b01b6e123aca846f742dde8a083a03c
Mike Georgeff <mike@georgeff.co>
2026-06-15 21:06:03 -0500

    add throw helpers to the kernel exception

--- src/KernelException.php
+++ src/KernelException.php
@@ -4,4 +4,24 @@ namespace Georgeff\Kernel;
 
 use RuntimeException;
 
-final class KernelException extends RuntimeException {}
+final class KernelException extends RuntimeException
+{
+    public static function throw(string $message): never
+    {
+        throw new self($message);
+    }
+
+    public static function throwIf(bool $condition, string $message): void
+    {
+        if ($condition) {
+            throw new self($message);
+        }
+    }
+
+    public static function throwIfNot(bool $condition, string $message): void
+    {
+        if (!$condition) {
+            throw new self($message);
+        }
+    }
+}
